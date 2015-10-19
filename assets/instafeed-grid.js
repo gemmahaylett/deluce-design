d5bae56ffa0d036aac7ac69c59f64df1eefb9c73
Gemma Haylett <gemmahaylett@gmail.com>
2015-10-18 21:49:48 -0700

    initial commit

---
+++ assets/instafeed-grid.js
@@ -0,0 +1,9 @@
+var feed = new Instafeed({
+  get : 'user',
+  userId: 1391335357,
+  accessToken: '1391335357.467ede5.7ba9271fabf643bc85ab081885d9033d',
+  resolution : 'low_resolution',
+  template: '<div class="grid__item insta-item large--one-fifth medium--one-fifth small--one-half"><a href="{{link}}"><img src="{{image}}" /></a></div>',
+  limit: '5'
+});
+feed.run();
