b42946ec446297db39fdcf32999967e58ff22bb4
Gemma Haylett <gemmahaylett@gmail.com>
2015-10-20 07:50:34 -0700

    homepage styles

--- assets/instafeed-grid.js
+++ assets/instafeed-grid.js
@@ -3,7 +3,7 @@ var feed = new Instafeed({
   userId: 1391335357,
   accessToken: '1391335357.467ede5.7ba9271fabf643bc85ab081885d9033d',
   resolution : 'low_resolution',
-  template: '<div class="grid__item insta-item large--one-fifth medium--one-fifth small--one-half"><a href="{{link}}"><img src="{{image}}" /></a></div>',
+  template: '<div class="grid__item insta-item large--one-fifth medium--one-fifth small--one-half"><a href="{{link}}"><div class="overlay"><img src="{{image}}" /></a></div></div>',
   limit: '5'
 });
 feed.run();
