daf8fcb059ae1595240dcd9cbc1cae83c6af4693
Gemma Haylett <gemmahaylett@gmail.com>
2015-10-29 20:11:24 -0700

    newsletter responsive styles and start of account pages

--- assets/instafeed-grid.js
+++ assets/instafeed-grid.js
@@ -3,7 +3,7 @@ var feed = new Instafeed({
   userId: 1391335357,
   accessToken: '1391335357.467ede5.7ba9271fabf643bc85ab081885d9033d',
   resolution : 'low_resolution',
-  template: '<div class="grid__item insta-item large--one-fifth medium--one-fifth small--one-half"><a href="{{link}}"><div class="overlay"><img src="{{image}}" /></a></div></div>',
+  template: '<div class="grid__item insta-item large--one-fifth medium--one-fifth small--one-half"><a class="tint" href="{{link}}" target="_blank"><img src="{{image}}" /></a></div>',
   limit: '5'
 });
 feed.run();
